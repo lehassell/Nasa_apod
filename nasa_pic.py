6bdfcf335f871c0bac219f6f942879e72584ad92
lehassell <lehassell@gmail.com>
2015-12-08 11:36:52 -0500

    Fixed non image and ascii bugs

--- nasa_pic.py
+++ nasa_pic.py
@@ -11,15 +11,31 @@ def nasa_getter(local_date, local_data, local_image):
     nasa_apod = requests.get(url, stream=True)
     nasa_pic = nasa_apod.json()
     response = requests.get(nasa_pic['hdurl'], stream=True)
-    nasa_data = open(local_data, 'a')
-    nasa_data.write(local_date)
-    nasa_data.write("\n")
-    nasa_data.write(nasa_pic['explanation'])
-    nasa_data.write("\n")
-    nasa_data.close()
-    with open(local_image, 'wb') as out_file:
-        shutil.copyfileobj(response.raw, out_file)
-    del response
+
+    # I want to check if its an image or other media type.
+    if nasa_pic['media_type'] == 'image':
+        # Stream out an IO for the picture
+        with open(local_image, 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+
+        # I then want to log the explanation.  This still has problems with non ascii characters
+        nasa_data = open(local_data, 'a')
+        nasa_data.write(local_date)
+        nasa_data.write("\n")
+
+        try:
+            nasa_data.write(nasa_pic['explanation'])
+            nasa_data.write("\n")
+        except UnicodeEncodeError:
+            nasa_data.write("Non ASCII Characters")
+            nasa_data.write("\n")
+            pass
+
+        nasa_data.close()
+        del response
+    else:
+        print nasa_pic['media_type']
+
     return()
 
 
@@ -36,7 +52,4 @@ if __name__ == '__main__':
         nasa_getter(date, nasa_data_file, nasa_img)
     else:
         print 'present'
-
-    # Test
-
 
