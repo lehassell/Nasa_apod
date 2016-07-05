99b08013d6ec370b7b4ca8def91f979dc82f2f9b
Hassell <test@test.com>
2016-07-05 10:00:13 -0400

    Added login screen changer and prints explanation of photo to terminal

--- nasa_pic.py
+++ nasa_pic.py
@@ -1,7 +1,9 @@
 import requests
 import shutil
 import subprocess
+from os import walk
 from datetime import date
+from random import randrange
 
 
 def nasa_getter(local_date, local_data, local_image):
@@ -19,6 +21,7 @@ def nasa_getter(local_date, local_data, local_image):
         with open(local_image, 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
 
+
         # I then want to log the explanation.  This still has problems with non ascii characters
         nasa_data = open(local_data, 'a')
         nasa_data.write(local_date)
@@ -26,6 +29,7 @@ def nasa_getter(local_date, local_data, local_image):
 
         try:
             nasa_data.write(nasa_pic['explanation'])
+            print(nasa_pic['explanation'])
             nasa_data.write("\n")
         except UnicodeEncodeError:
             nasa_data.write("Non ASCII Characters")
@@ -35,27 +39,43 @@ def nasa_getter(local_date, local_data, local_image):
         nasa_data.close()
         del response
     else:
-        print nasa_pic['media_type']
-
+        print(nasa_pic['media_type'])
     return()
 
+def login_screen(login_folder):
+    " Function to automate randomizing login screen photo"
+    images =[]
+    for (dirpath, dirnames, filenames) in walk(login_folder):
+        images.extend(filenames)
+    images.pop(0)
+    image_number = len(images)
+    x = randrange(0,image_number,1)
+    image_choice = login_folder + images[x]
+    print(image_choice)
+    subprocess.call(['cp', image_choice, '/Library/Caches/com.apple.desktop.admin.png'])
+    return()
 
 if __name__ == '__main__':
     # Check the date
     date = str(date.today())
-    # Determine the file location
+    # Determine the nasa file location
     nasa_dir = "/Users/lehassell/Pictures//Nasa/"
     nasa_data_file = nasa_dir + 'data.txt'
     nasa_img = "/Users/lehassell/Pictures//Nasa/" + date + "_nasa.png"
+
+    # Login screen dir
+    login_dir = "/Users/lehassell/Pictures/Wallpaper/"
 
     # Don't duplicate if its been run before
     if date not in open(nasa_data_file).read():
         nasa_getter(date, nasa_data_file, nasa_img)
     else:
-        print 'present'
-
+        print('present')
 
     # Set the image as background
-    place_string = 'tell application "Finder" to set desktop picture to POSIX file "'+ nasa_img +'"'
+    place_string = 'tell application "Finder" to set desktop picture to POSIX file "' + nasa_img + '"'
     subprocess.call(['osascript', '-e', place_string])
 
+
+    # Set login screen to random
+    login_screen(login_dir)
